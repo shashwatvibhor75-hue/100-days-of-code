9948a10577ff6ebc8c9598d089ef88e72f678324
Shashwat Vibhor <shashwatvibhor75@gmail.com>
2026-09-11 10:17:25 +0530

    commit

--- day1_q2.c
+++ day1_q2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int  main()
 {
-    int
+    int n1, n2, sum, pro, diff;
+    double div;
+    printf("enter 2 numbers");
+    scanf("%d %d", &n1, &n2);
+    sum = n1 + n2;
+    pro = n1 * n2;
+    diff = n1 - n2;
+    div = n1/n2;
+    printf("sum is %d\n", sum);
+    printf("product is %d\n", pro);
+    printf("difference is %d\n", diff);
+    printf("quotient is %.2f\n", div);
+    return 0;
+
 }
